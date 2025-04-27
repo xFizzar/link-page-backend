488ed1659ab4150e19f1971b4358b4ddfb8bc9d1
BuildTools <unconfigured@null.spigotmc.org>
2025-04-27 22:44:36 +0200

    temp fix @CrossOrigin

--- src/main/java/dev/fizzar/linkpagebackend/api/RestAPI.java
+++ src/main/java/dev/fizzar/linkpagebackend/api/RestAPI.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = {"http://localhost:4200", "http://10.0.0.205:4200"})
 public record RestAPI(AppService appService) {
 
     @GetMapping("apps")
